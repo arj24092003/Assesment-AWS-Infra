c1a7de5f7fd3774d6fb79e6677c5f2d5507b2950
arj24092003 <144003117+arj24092003@users.noreply.github.com>
2026-07-07 22:02:06 +0530

    Initialize Flask application with health check routes

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "status":"Application Running"
+    }
+
+@app.route("/health")
+def health():
+    return {
+        "health":"OK"
+    }
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
